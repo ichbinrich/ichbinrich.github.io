eb7c22b79f5b74c0925cc01fab895d0a9c064144
Ricardo Martinez <ing.ricardo.martinez@gmail.com>
2023-01-25 17:38:36 +0100

    Update html and js for typewriter loop

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+let message = "For further information, contact me!";
+let id = "text";
+let delay = 150;
 function sleep(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 async function typeWriter(id, message, delay) {
-  for ( i=0; i < message.length; i++) {
+  for (i = 0; i < message.length; i++) {
     document.getElementById(id).innerHTML += message.charAt(i);
     await sleep(delay);
   }
+  await sleep(delay*3)
+  document.getElementById(id).innerHTML = ""
+  typeWriter(id, message, delay)
 }
+
+typeWriter(id,message,delay)
