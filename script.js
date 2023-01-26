21fc7a1dd343382898b1675722f6994047cf049e
Ricardo Martinez <ing.ricardo.martinez@gmail.com>
2023-01-26 10:19:26 +0100

    Change speed in animation text

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let message = "For further information, contact me!";
 let id = "text";
 let delay = 150;
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, Math.random() * 5 * ms));
 }
 async function typeWriter(id, message, delay) {
   for (i = 0; i < message.length; i++) {
